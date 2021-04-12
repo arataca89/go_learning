2514b7d12b86a91d1b9d2ee9afa3cbfd3cb50880
arataca89 <75431558+arataca89@users.noreply.github.com>
2021-04-12 19:48:56 -0300

    Add files via upload

---
+++ primo.go
@@ -0,0 +1,29 @@
+// primo.go
+// verifica se um número é primo
+// arataca89@gmail.com
+// 20210412
+package main
+
+import "fmt"
+
+func main() {
+	var nr int
+	var divisor int
+	isprime := true
+
+	fmt.Print("Entre com o número: ")
+	fmt.Scanf("%d", &nr)
+
+	for divisor = 2; divisor < nr; divisor++ {
+		if nr%divisor == 0 {
+			isprime = false
+			break
+		}
+	}
+
+	if isprime == true {
+		fmt.Println("O número", nr, "é primo")
+	} else {
+		fmt.Println("O número", nr, "não é primo")
+	}
+}
